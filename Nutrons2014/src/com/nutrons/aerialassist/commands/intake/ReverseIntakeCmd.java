fe5b17555238f2195df85c0b3e123cf27823248f
nutronspros <NUTRONs@NUTRONs-PC.(none)>
2014-02-02 15:08:47 -0500

    Tank drive and intake work. Need to fix cheesy.

---
+++ Nutrons2014/src/com/nutrons/aerialassist/commands/intake/ReverseIntakeCmd.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nutrons.aerialassist.commands.intake;
+
+import com.nutrons.aerialassist.commands.CommandBase;
+
+/**
+ *
+ * @author NUTRONs
+ */
+public class ReverseIntakeCmd extends CommandBase {
+
+    public ReverseIntakeCmd() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(intake);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        intake.setMotorSpeed(-1.0);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        intake.setMotorSpeed(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        this.end();
+    }
+}
