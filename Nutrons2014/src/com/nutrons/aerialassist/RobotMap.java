e5b2eb007082c03f3eb37695ebcbc1c00233c6ed
nutronspros <NUTRONs@NUTRONs-PC.(none)>
2014-04-09 21:01:02 -0400

    Switched gate latches to channel 5.

--- Nutrons2014/src/com/nutrons/aerialassist/RobotMap.java
+++ Nutrons2014/src/com/nutrons/aerialassist/RobotMap.java
@@ -46,10 +46,10 @@ public class RobotMap {
 //    Pneumatics
     public static final int INTAKE_PISTON = 7;
     public static final int INTAKE_UP_PISTON = 8;
-    public static final int WING = 5;
+    public static final int WING = 3;
     public static final int CLAMPS = 4;
     public static final int CANDY_CANE = 6;
-    public static final int GATE_LATCHES = 3;
+    public static final int GATE_LATCHES = 5;
 //    Relays
     public static final int COMPRESSOR_PORT = 3;
 //    ANALOG INPUTS
